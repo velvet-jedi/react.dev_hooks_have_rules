df5a4e8a8d067c36afcaae922dd1c27da7554fa4
velvet-jedi <132247456+velvet-jedi@users.noreply.github.com>
2024-04-06 08:47:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+/*
+Hooks can only be called from the top level of a functional component.
+Hooks can’t be called from inside loops or conditions.
+*/
+import { useState } from 'react';
+
+export default function FeedbackForm() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState('');
+  if (isSent) {
+    return <h1>Thank you!</h1>;
+  } else {
+    return (
+      <form onSubmit={e => {
+        e.preventDefault();
+        alert(`Sending: "${message}"`);
+        setIsSent(true);
+      }}>
+        <textarea
+          placeholder="Message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        />
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    );
+  }
+}
